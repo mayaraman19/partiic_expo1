8a2c38032b5bc203d93e7a1c00082bf9d8f807d0
Chelsey Wang <chelseyyywang@gmail.com>
2020-02-14 09:40:20 -0800

    map screen

--- screens/Map.js
+++ screens/Map.js
@@ -20,25 +20,22 @@ import { AuthSession } from 'expo';
 export default function Map() {
     return (
         <View style = {styles.background}>
-            <TouchableOpacity /*key = {index} */ style = {styles.policeButton}
-            /*onPress={()=>{this.dialCall(4085477683)}}*/>
-                <Text style = {styles.policeText}>MAPMAPMAP</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style = {styles.button}>
-                <Text style = {styles.buttonText}>
-                    UCPD
-                </Text>
-            </TouchableOpacity>
-            {/*<TouchableOpacity style = {styles.button}>
-                <Text style = {styles.buttonText}>
-                    UCPD Anonymous
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style = {styles.button}>
-                <Text style = {styles.buttonText}>
-                    Evening Escort Service
-                </Text>
-        </TouchableOpacity>*/}
+            <Image source={require('../components/pics/map.png')} />
+            <View style = {styles.row}>
+                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Parties Today</Text></TouchableOpacity>
+                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>1 Mile</Text></TouchableOpacity>
+                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Drinks</Text></TouchableOpacity>
+                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Entry</Text></TouchableOpacity>
+            </View>
+            <View style = {styles.row}>
+                <TouchableOpacity><Text style = {styles.eventTitle}>1. Fundrager</Text></TouchableOpacity>
+                <View style = {{width: "20%", flexDirection: "row", justifyContent: "space-evenly"}}>
+                    <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>L</Text></TouchableOpacity>
+                    <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>F</Text></TouchableOpacity>
+                </View>
+                
+            </View>
+
         </View>
     );
 }
@@ -46,67 +43,30 @@ export default function Map() {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        backgroundColor: '#050A39',
+        backgroundColor: 'black',
         alignItems: 'center',
-        padding: 100,
     },
-    policeButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        width: 340,
-        height: 55,
-        top: 156,
-        backgroundColor: 'rgba(244, 73, 73, 0.8)',
-        borderRadius: 11,
+    row: {
+        backgroundColor: "red",
+        width: "100%",
+        justifyContent: "space-between",
+        display: "flex", 
+        flexDirection: "row",
+        paddingLeft: 20, 
+        paddingRight: 20, 
+        paddingTop: 10, 
+        paddingBottom: 10,
     },
-    policeText: {
-        position: 'absolute',
-        /*left: 4.8,
-        right: 4.53%,
-        top: 21.06%,
-        bottom: 75.99%,*/
-
-        //font-family: SF Pro Display;
-        fontStyle: 'italic',
-        fontWeight: 'bold',
-        fontSize: 24,
-        lineHeight: 29,
-        textAlign: 'center',
-        letterSpacing: 0.2,
-
-        color: '#FFFFFF',
-
-        opacity: 0.9,
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        width: 340,
-        height: 55,
-        top: 260,
-        bottom: 100,
-
-        backgroundColor: '#19DAD4',
-        borderRadius: 11,
+    filterButton: {
+        borderWidth: 1, 
+        borderRadius: 20,
+        borderColor: "white",
     },
     buttonText: {
-        position: 'absolute',
-        /*left: 11.2%;
-        right: 11.2%;
-        top: 44.21%;
-        bottom: 52.83%;*/
-        //font-family: SF Pro Display;
-        fontStyle: 'normal',
-        fontWeight: 'bold',
-        fontSize: 18,
-        lineHeight: 21,
-        textAlign: 'center',
-        letterSpacing: 0.1,
-
-        color: '#FFFFFF',
-
-        opacity: 0.9,
+        color: "white", 
+        padding: 5, 
     },
+    eventTitle: {
+        fontWeight: "bold", fontSize: 18, color: "white"
+    }
 });
