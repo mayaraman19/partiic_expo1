d4a60dcda284bd6f134b743d2503121251f2d4f8
Chelsey Wang <chelseyyywang@gmail.com>
2020-03-04 17:51:41 -0800

    borrowed a movies endpoint now party names are movie names

--- screens/Map.js
+++ screens/Map.js
@@ -10,38 +10,90 @@ import {
   View,
   Dimensions,
   ScrollView,
+  ActivityIndicator
 } from 'react-native';
 import { AuthSession } from 'expo';
 import PartyListItem from '../components/PartyListItem';
 import MapPin from '../components/MapPin';
+import { tsConstructorType } from '@babel/types';
 
 
-export default function Map({navigation}) {
-    return (
-        <View style = {styles.background}>
-            {/* <Image source={require('../components/pics/map.png')} /> */}
-            <MapView style={styles.mapStyle} region={{latitude: 34.06, longitude: -118.44, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}>
-            {/* <Marker coordinate={{latitude: 34.06, longitude: -118.44}} >
-                <Callout>
-                    <Text> hlkjgjhkmb,mnbiglhbm, </Text>
-                </Callout>
-            </Marker> */}
-            <MapPin partyName="hello" partyAddress="address" partyDate="insert date here"/>
-            </MapView>
-            <View style = {styles.row}>
-                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Parties Today</Text></TouchableOpacity>
-                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>1 Mile</Text></TouchableOpacity>
-                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Drinks</Text></TouchableOpacity>
-                <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Entry</Text></TouchableOpacity>
-            </View>
-            <ScrollView>
-                <PartyListItem partyName=";lskfdj;alskfhs;" partyAddress="Gayley" partyDate="date"/>
-                <PartyListItem partyName=";lskfdj;alskfhs;" partyAddress="Gayley" partyDate="date"/>
-                <PartyListItem partyName=";lskfdj;alskfhs;" partyAddress="Gayley" partyDate="date"/>
-            </ScrollView>
+// export default function Map({navigation}) {
+    
+export default class Map extends React.Component {  
+    
+    constructor(props){
+        super(props); 
+        this.state = {
+            isLoading: true, 
+            dataSource: null, 
+        }
+    }
+    
+    componentDidMount(){
 
-        </View>
-    );
+        return fetch('https://facebook.github.io/react-native/movies.json')
+            .then( (response) => response.json() )
+            .then( (responseJson) => {
+
+                this.setState(
+                    {
+                        isLoading: false, 
+                        dataSource: responseJson.movies,
+                    }
+                )
+
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+    }
+    
+    render(){
+
+        if (this.state.isLoading){
+            return(
+                <View><ActivityIndicator /></View>
+            )
+        }
+        else
+        {
+            let movies = this.state.dataSource.map((val, key) => {
+                return <View key={key}><PartyListItem partyName={val.title} partyAddress="Gayley" partyDate="date"/></View>
+            })
+
+
+            // what u gonna see on the screen baby
+            return (
+                <View style = {styles.background}>
+                    <MapView style={styles.mapStyle} region={{latitude: 34.06, longitude: -118.44, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}>
+                    <MapPin partyName="hello" partyAddress="address" partyDate="insert date here"/>
+                    </MapView>
+                    <View style = {styles.row}>
+                        <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Parties Today</Text></TouchableOpacity>
+                        <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>1 Mile</Text></TouchableOpacity>
+                        <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Drinks</Text></TouchableOpacity>
+                        <TouchableOpacity style = {styles.filterButton}><Text style = {styles.buttonText}>Free Entry</Text></TouchableOpacity>
+                    </View>
+                    <ScrollView>
+                        {movies}
+                        {/* <PartyListItem partyName={movies} partyAddress="Gayley" partyDate="date"/>
+                        <PartyListItem partyName=";lskfdj;alskfhs;" partyAddress="Gayley" partyDate="date"/>
+                        <PartyListItem partyName=";lskfdj;alskfhs;" partyAddress="Gayley" partyDate="date"/>
+                        {movies}
+                        {movies[0]} */}
+                    </ScrollView>
+
+                </View>
+            );
+        }
+
+
+    
+
+
+    } //render
 }
 
 const styles = StyleSheet.create({
